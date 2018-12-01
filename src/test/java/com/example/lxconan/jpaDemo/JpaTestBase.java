a258f75cb7817160d04c238db346411f4eb8ac23
Liu Xia <lxconan@gmail.com>
2018-12-01 10:52:34 +0800

    [liuxia] refactor: create test base class.

---
+++ src/test/java/com/example/lxconan/jpaDemo/JpaTestBase.java
@@ -0,0 +1,31 @@
+package com.example.lxconan.jpaDemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import java.util.function.Consumer;
+
+public abstract class JpaTestBase {
+    @Autowired
+    private EntityManager entityManager;
+
+    protected EntityManager getEntityManager() {return entityManager;}
+
+    protected void prepareDataAndFlush(Consumer<EntityManager> consumer) {
+        final EntityManager em = getEntityManager();
+        consumer.accept(em);
+        em.flush();
+    }
+
+    public void runAndClear(Consumer<EntityManager> consumer) {
+        final EntityManager em = getEntityManager();
+        consumer.accept(em);
+        em.flush();
+        em.clear();
+    }
+
+    public void runJpa(Consumer<EntityManager> consumer) {
+        final EntityManager em = getEntityManager();
+        consumer.accept(em);
+    }
+}
--- src/test/java/com/example/lxconan/jpaDemo/manyToOne/ManyToOneTests.java
+++ src/test/java/com/example/lxconan/jpaDemo/manyToOne/ManyToOneTests.java
@@ -1,33 +1,25 @@
 package com.example.lxconan.jpaDemo.manyToOne;
 
+import com.example.lxconan.jpaDemo.JpaTestBase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-
-import static com.example.lxconan.jpaDemo.JpaHelper.prepareDataAndFlush;
-import static com.example.lxconan.jpaDemo.JpaHelper.runAndClear;
-import static com.example.lxconan.jpaDemo.JpaHelper.runJpa;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class ManyToOneTests {
-    @Autowired
-    private EntityManager entityManager;
-
+public class ManyToOneTests extends JpaTestBase {
     @Test
     public void should_persist_post_commend_with_existing_post() {
-        prepareDataAndFlush(entityManager, em -> {
+        prepareDataAndFlush(em -> {
             final Post post = new Post(1L, "title");
             em.persist(post);
         });
 
         // When
-        runAndClear(entityManager, em -> {
+        runAndClear(em -> {
             final Post post = em.find(Post.class, 1L);
             final PostComment comment = new PostComment(1L, "review");
             comment.setPost(post);
@@ -35,9 +27,9 @@ public class ManyToOneTests {
         });
 
         // Then
-        runJpa(entityManager, em -> {
-            final PostComment postComment = entityManager.find(PostComment.class, 1L);
-            final Post post = entityManager.find(Post.class, 1L);
+        runJpa(em -> {
+            final PostComment postComment = em.find(PostComment.class, 1L);
+            final Post post = em.find(Post.class, 1L);
 
             assertThat(postComment.getReview())
                 .isEqualTo("review");
